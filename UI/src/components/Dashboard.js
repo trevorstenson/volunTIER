7a64d00e2e459d0ea467a499b1afef0e41a2771f
Rahul Suryadevara <rahul_suryadevara@yahoo.com>
2019-02-09 21:05:11 -0500

    project

---
+++ UI/src/components/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './bootstrap.css';
+import PointsCard from './PointsCard';
+import HoursCard from './HoursCard';
+import FriendsList from './FriendsList';
+import FeaturedOpportunities from './FeaturedOpprtunities';
+import ActivityFeed from './ActivityFeed';
+import NavBar from './NavBar';
+
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+
+        );
+    }
+
+}
+
+export default Dashboard;
